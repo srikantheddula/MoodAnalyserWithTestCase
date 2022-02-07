318e54e7963d91981c847556cb977941f7a163dc
srikantheddula <you@example.com>
2022-02-07 22:27:14 +0530

    [srikanth]:adding welocme mesg to moodanalyser

---
+++ MoodAnalyzerWithTestCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerWithTestCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the MoodAnalyzer with test case");
+        }
+    }
+}
